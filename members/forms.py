e825d1f772429e1cfae89ab6c8e2ce79ef5190d5
joy-mahmud <joymahmudiucse@gmail.com>
2024-11-14 22:54:22 +0600

    model form redirect member details

--- members/forms.py
+++ members/forms.py
@@ -1,4 +1,11 @@
 from django import forms
+from .models import Member
 
 class MemberSearchForm(forms.Form):
     query=forms.CharField()
+ 
+    
+class AddMemberForm(forms.ModelForm):
+    class Meta:
+        model=Member
+        fields=["firstname","lastname","age"]
--- members/views.py
+++ members/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from members.models import Member
-from members.forms import MemberSearchForm
+from members.forms import MemberSearchForm,AddMemberForm
 
 # Create your views here.
 
@@ -24,15 +24,25 @@ def members_about(request):
 
 def add_member(request):
    
-    print(request.GET)
-    firstname=request.GET.get("firstname")
-    lastname=request.GET.get("lastname")
-    age=request.GET.get("age")
-    if all([firstname,lastname,age]):
-        member= Member(firstname=firstname,lastname=lastname,age=age)
-        member.save()
+    #if request.method=="POST":
+        # firstname=request.POST.get("firstname")
+        # lastname=request.POST.get("lastname")
+        # age=request.POST.get("age")
+        # if all([firstname,lastname,age]):
+        #     member= Member(firstname=firstname,lastname=lastname,age=age)
+        #     member.save()
+        #     return HttpResponse("new member added successfully")
+    if request.method=="POST":
+       form=AddMemberForm(request.POST)
+       if form.is_valid():
+           form.save()
+           return redirect("view_members")
+    else:
+        form=AddMemberForm()
+        context={'form':form}
+        return render(request,"add_member.html",context)        
     
-    return render(request,"add_member.html")
+    
 
 def add_multiple_member(request):
      member1 = Member(firstname='Tobias', lastname='Refsnes')
@@ -65,6 +75,14 @@ def view_members(request):
         "allMembers":members
     }
     return render(request,'view_members.html',context) 
+def member_details(request,id):
+    member=Member.objects.get(id=id)
+    context={
+        "member_details":member
+    }
+    
+    return render(request,"member_details.html",context)
+    
 
 def search_member(request):
     django_form=MemberSearchForm(request.GET or None)
